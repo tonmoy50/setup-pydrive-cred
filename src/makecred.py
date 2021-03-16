b0fabf16b9eb6c78e925325050f0ffd12040ef6b
Nilambar Halder Tonmoy <tonmoyhalder50@gmail.com>
2021-03-17 01:38:44 +0600

    Added Code

---
+++ src/makecred.py
@@ -0,0 +1,5 @@
+from pydrive.auth import GoogleAuth
+
+gauth = GoogleAuth()
+gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+gauth.SaveCredentialsFile("mycreds.txt")
